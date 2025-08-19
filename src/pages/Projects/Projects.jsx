696e8ce909075425999913e515638aa523fd3e72
bigdevsircole <caleb.yinusa@gmail.com>
2025-08-19 15:33:14 +0100

    Initial commit

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -101,9 +101,7 @@ const Projects = () => {
               <img src={project.image} alt={project.title} />
               <h3>{project.title}</h3>
               <p>{project.description}</p>
-              <a href={project.link} target="_blank" rel="noopener noreferrer" className="view-project-link">
-                View Project
-              </a>
+              <a href={project.link} target="_blank" rel="noopener noreferrer" className="view-project-link">View Project</a>
             </div>
           ))}
         </div>
